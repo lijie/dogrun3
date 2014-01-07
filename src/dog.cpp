d47d8e091a27a89cf05b167def16c2313e756704
eltshanli <eltshanli@gmail.com>
2014-01-08 00:14:27 +0800

    add User.UseMoney

--- src/dog.cpp
+++ src/dog.cpp
@@ -3,66 +3,89 @@
 #include "cocos2d.h"
 #include "dog.h"
 
-Dog::Dog()
-{
-    lv_ = 1;
-    star_ = 1;
-    str_ = 1;
-    speed_ = 1;
-    intimacy_ = 1;
-    name_ = "FirstDog";
-    feedcd_ = 0;
-    traincd_ = 0;
+Dog::Dog() {
+  lv_ = 1;
+  star_ = 1;
+  str_ = 1;
+  speed_ = 1;
+  intimacy_ = 1;
+  name_ = "FirstDog";
+  feedcd_ = 0;
+  traincd_ = 0;
+  playcd_ = 0;
 }
 
-int Dog::Feed(int feedtype)
-{
+int Dog::Feed(int feedtype) {
   time_t now = time(NULL);
   if (feedcd_ > now) {
     CCLOGERROR("feed in cd.\n");
     return -1;
   }
 
-  intimacy_++;
-  feedcd_ = now + 60;
+  str_++;
+  exp_++;
+  feedcd_ = now + kDefalutFeedCD;
   return 0;
 }
 
-int Dog::Train(int traintype)
-{
+int Dog::Train(int traintype) {
   time_t now = time(NULL);
   if (traincd_ > now) {
     CCLOGERROR("Train in cd.\n");
     return -1;
   }
 
-  str_++;
-  traincd_ = now + 60;
+  speed_++;
+  exp_++;
+  traincd_ = now + kDefaultTrainCD;
+  return 0;
+}
+
+int Dog::Play(int playtype) {
+  time_t now = time(NULL);
+  if (playcd_ > now) {
+    CCLOGERROR("Play in cd.\n");
+    return -1;
+  }
+
+  intimacy_++;
+  exp_++;
+  playcd_ = now + kDefaultPlayCD;
+  return 0;
+}
+
+int Dog::Levelup() {
   return 0;
 }
 
 User* User::current_;
 
-void User::InitCurrent()
-{
-    User *u = new User();
-    assert(u != NULL);
+void User::InitCurrent() {
+  User *u = new User();
+  assert(u != NULL);
 
-    u->set_id("FirstUser");
-    u->set_title("¼ªÍÞÍÞÎ¹Ñø×¨¼Ò");
-    u->set_money(1000);
-    u->set_heart(1000);
+  u->set_id("FirstUser");
+  u->set_title("å‰å¨ƒå¨ƒå–‚å…»ä¸“å®¶");
+  u->set_money(1000);
+  u->set_heart(1000);
 
-    Dog *dog = new Dog();
-    assert(u != NULL);
-    u->AddDog(dog);
-    dog->set_owner(u);
+  Dog *dog = new Dog();
+  assert(u != NULL);
+  u->AddDog(dog);
+  dog->set_owner(u);
 
-    User::set_current(u);
+  User::set_current(u);
 }
 
-int User::AddDog(Dog *dog)
-{
-    dogs_.push_back(dog);
-    return 0;
+int User::AddDog(Dog *dog) {
+  dogs_.push_back(dog);
+  return 0;
+}
+
+int User::UseMoney(int money) {
+  if (money_ < money)
+    return -1;
+
+  money_ -= money;
+  return 0;
 }
--- src/dog.h
+++ src/dog.h
@@ -7,64 +7,86 @@
 using std::string;
 using std::vector;
 
+const int kDefalutFeedCD = 60;
+const int kDefaultTrainCD = 60;
+const int kDefaultPlayCD = 60;
+
+enum {
+  kDogStarD = 1,
+  kDogStarC = 2,
+  kDogStarB = 3,
+  kDogStarA = 4,
+  kDogStarS = 5,
+  kDogStarSS = 6,
+  kDogStarSSS = 7,
+};
+
 class Dog;
 
 class User {
 public:
-    // attr
-    const string& id() {return id_;};
-    const string& title();
-    int money();
-    int heart();
+  // attr
+  const string& id() {return id_;};
+  const string& title();
+  int money();
+  int heart();
 
-    static User *current() {return current_;};
-    static void InitCurrent();
-    static void set_current(User *u) {current_ = u;}
+  static User *current() {return current_;};
+  static void InitCurrent();
+  static void set_current(User *u) {current_ = u;}
 
-    void set_id(const string& id) {id_ = id;}
-    void set_title(const string& title) {title_ = title;}
-    void set_money(int money) {money_ = money;}
-    void set_heart(int heart) {heart_ = heart;}
+  void set_id(const string& id) {id_ = id;}
+  void set_title(const string& title) {title_ = title;}
+  void set_money(int money) {money_ = money;}
+  void set_heart(int heart) {heart_ = heart;}
 
-    int AddDog(Dog *dog);
-    Dog *dogs(int idx) {return dogs_[idx];}
-    size_t dogs_count(void) {return dogs_.size();}
+  int AddDog(Dog *dog);
+  Dog *dogs(int idx) {return dogs_[idx];}
+  size_t dogs_count(void) {return dogs_.size();}
+
+  int UseMoney(int money);
 protected:
-    string id_;
-    string title_;
-    int money_;
-    int heart_;
+  string id_;
+  string title_;
+  int money_;
+  int heart_;
 
-    vector<Dog *> dogs_;
-    static User* current_;
+  vector<Dog *> dogs_;
+  static User* current_;
 };
 
 class Dog {
 public:
-    Dog();
-    virtual int Feed(int foodtype);
-    virtual int Train(int traintype);
+  Dog();
+  virtual int Feed(int foodtype);
+  virtual int Train(int traintype);
+  virtual int Play(int playtype);
 
-    // attr
-    const string& name();
-    int lv();
-    int star();
-    int str();
-    int speed();
-    int intimacy();
-    User *owner();
+  // attr
+  const string& name() {return name_;}
+  int lv() {return lv_;}
+  int star() {return star_;}
+  int str() {return str_;}
+  int speed() {return speed_;}
+  int intimacy() {return intimacy_;}
+  int exp() {return exp_;}
+  User *owner();
 
-    // set methods for attr
-    void set_owner(User *owner) {owner_ = owner;}
+  // set methods for attr
+  void set_owner(User *owner) {owner_ = owner;}
 protected:
-    string name_;
-    int lv_;
-    int star_;
-    int str_;
-    int speed_;
-    int intimacy_;
-    time_t feedcd_;
-    time_t traincd_;
-    User *owner_;
+  string name_;
+  int lv_;
+  int exp_;
+  int star_;
+  int str_;
+  int speed_;
+  int intimacy_;
+  time_t feedcd_;
+  time_t traincd_;
+  time_t playcd_;
+  User *owner_;
+
+  int Dog::Levelup();
 };
 #endif // ! __DOGRUN2_DOG__
